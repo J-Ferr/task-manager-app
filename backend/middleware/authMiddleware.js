ccbabb5a928c94c03a25e6dec5a5872aa4d99967
Jose Armando <Arellano.53j@gmail.com>
2025-12-04 20:03:36 -0500

    created authMiddleware file

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const requireAuth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ error: "No token provided" });
+    }
+
+    const token = authHeader.split(" ")[1]; // "Bearer token"
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded; // attach user info (like id)
+        next();
+    } catch (err) {
+        return res.status(403).json({ error: "Invalid token" });
+    }
+};
+
+module.exports = requireAuth;
